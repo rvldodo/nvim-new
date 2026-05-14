0f61ccfe95252412781c271eaed1372f17436dea
rvldodo <nono.qwerty28@gmail.com>
2026-05-14 19:04:45 +0700

    feat: 14 may 2026

--- lua/rvldodo/plugins/lsp/mason.lua
+++ lua/rvldodo/plugins/lsp/mason.lua
@@ -1,61 +1,81 @@
 return {
-   "williamboman/mason.nvim",
-   dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-   },
-   config = function()
-      -- import mason
-      local mason = require("mason")
-
-      -- import mason-lspconfig
-      local mason_lspconfig = require("mason-lspconfig")
-
-      local mason_tool_installer = require("mason-tool-installer")
-
-      -- enable mason and configure icons
-      mason.setup({
-         ui = {
-            icons = {
-               package_installed = "✓",
-               package_pending = "➜",
-               package_uninstalled = "✗",
+   {
+      "williamboman/mason.nvim",
+      build = ":MasonUpdate",
+      config = function()
+         require("mason").setup({
+            ui = {
+               icons = {
+                  package_installed = "✓",
+                  package_pending = "➜",
+                  package_uninstalled = "✗",
+               },
             },
-         },
-      })
+         })
+      end,
+   },
 
-      mason_lspconfig.setup({
-         -- list of servers for mason to install
-         ensure_installed = {
-            -- "rust-analyzer",
-            "ts_ls",
-            "html",
-            "cssls",
-            "tailwindcss",
-            "svelte",
-            "lua_ls",
-            "graphql",
-            "gopls",
-            "emmet_ls",
-            "prismals",
-            "pyright",
-            -- "proto",
-         },
-      })
+   {
+      "williamboman/mason-lspconfig.nvim",
+      dependencies = {
+         "williamboman/mason.nvim",
+         "neovim/nvim-lspconfig", -- 🔥 REQUIRED
+      },
+      config = function()
+         local mason_lspconfig = require("mason-lspconfig")
+         local lspconfig = require("lspconfig")
 
-      mason_tool_installer.setup({
-         ensure_installed = {
-            "biome",
-            "prettier", -- prettier formatter
-            "stylua", -- lua formatter
-            "isort", -- python formatter
-            "black", -- python formatter
-            "pylint", -- python linter
-            "eslint_d", -- js linter
-            "golines",
-            "gomodifytags",
-            "protolint",
-         },
-      })
-   end,
+         mason_lspconfig.setup({
+            ensure_installed = {
+               "ts_ls",
+               "html",
+               "cssls",
+               "tailwindcss",
+               "svelte",
+               "lua_ls",
+               "graphql",
+               "gopls",
+               "emmet_ls",
+               "prismals",
+               "pyright",
+               "rust_analyzer", -- ⚠️ FIX NAME
+            },
+         })
+
+         -- 🔥 auto setup all servers
+         mason_lspconfig.setup_handlers({
+            function(server_name)
+               lspconfig[server_name].setup({})
+            end,
+         })
+      end,
+   },
+
+   {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      dependencies = { "williamboman/mason.nvim" },
+      config = function()
+         require("mason-tool-installer").setup({
+            ensure_installed = {
+               "biome",
+               "prettier",
+               "stylua",
+               "isort",
+               "black",
+               "pylint",
+               "ruff",
+               "eslint_d",
+               "golines",
+               "gomodifytags",
+               "protolint",
+               "google-java-format",
+               "ktlint",
+               "checkstyle",
+               "sqlfluff",
+            },
+            auto_update = false,
+            run_on_start = true, -- 🔥 IMPORTANT
+         })
+      end,
+   },
 }
